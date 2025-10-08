00d59a8acf83a7db4cb7036285bf6e54bf8ea03c
25UG051176 <shreejeetkushwah@gmail.com>
2025-10-08 18:47:21 +0530

    Create Butterfly_pattern.py

---
+++ Butterfly_pattern.py
@@ -0,0 +1,28 @@
+n=int(input("Enter any no."))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    
+    for j in range(n-i-1):
+        print(" ",end=" ")
+        
+    for j in range(n-i-1):
+        print(" ",end=" ")
+        
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
+for i in range(n):
+    for j in range(n-i):
+        print("*",end=" ")
+    
+    for j in range(i):
+        print(" ",end=" ")
+        
+    for j in range(i):
+        print(" ",end=" ")
+        
+    for j in range(n-i):
+        print("*",end=" ")
+    print()
